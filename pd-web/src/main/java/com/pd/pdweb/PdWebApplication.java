d07884a352001d1a7852a04b969f3802a10399ed
980187223@qq.com <Syp950628>
2020-09-04 17:44:46 +0800

    初始化pd_store_search

---
+++ pd-web/src/main/java/com/pd/pdweb/PdWebApplication.java
@@ -0,0 +1,13 @@
+package com.pd.pdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdWebApplication.class, args);
+    }
+
+}
